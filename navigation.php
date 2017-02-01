ae1c0de4e5f22ce8804215b8726f88eab39f7500
Dillon <dtorgersen86@gmail.com>
2017-01-31 20:47:47 -0700

    First test
    Just a test...

---
+++ navigation.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+<div id="Menu">
+    <a href="index.php">Home</a> | <a href="devices.php">Devices</a> | <a href="status.php">Status</a> | <a href="reports.php">Reports</a>
+</div>
